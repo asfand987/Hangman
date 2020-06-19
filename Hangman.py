e60fc85008ca3587d0bcb62b29845d0e4fec3fb4
asfand987 <asfand987.ak@gmail.com>
2020-06-19 19:15:12 +0100

    Added words the user can choose from

---
+++ Hangman.py
@@ -0,0 +1,11 @@
+import random, time
+
+#words to choose from.
+
+fruits = ['pear', 'mango', 'apple', 'banana', 'apricot', 'pineapple','cantaloupe', 'grapefruit','jackfruit','papaya'] 
+superHeroes = ['hawkeye', 'robin', 'Galactus', 'thor', 'mystique', 'superman', 'deadpool', 'vision', 'sandman']
+
+userGuesslist = []
+userGuesses = []
+playGame = True
+
