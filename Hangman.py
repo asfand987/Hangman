201de19ac3fb8c6fe4abe6294ccaebdd6c8be885
asfand987 <asfand987.ak@gmail.com>
2020-06-22 18:12:27 +0100

    Added a menu with difficulties

--- Hangman.py
+++ Hangman.py
@@ -1,28 +1,40 @@
-import random, time
+import random
 
-#words to choose from.
+#list of words for game
+hangmanWords = ("Halloween","Hockey","Minnesota","Vikings","Twins","Timberwolves","Wild","Playstation","Achievement","Minecraft","Metallica","Portal","Xbox","Guitar")
 
-fruits = ['pear', 'mango', 'apple', 'banana', 'apricot', 'pineapple','cantaloupe', 'grapefruit','jackfruit','papaya'] 
-superHeroes = ['hawkeye', 'robin', 'Galactus', 'thor', 'mystique', 'superman', 'deadpool', 'vision', 'sandman']
+#assigns radomized word to variable
+randomWord = random.choice(hangmanWords)
+3
+def menu():
+   print("""
+Welcome to Hangman!
 
-randomWord = random.choice(fruits)
+Select a difficulty:
+1. Easy (9 Misses)
+2. Medium (7 Misses)
+3. Advanced (5 Misses)
 
+4. Exit Game
+""")
+   selection = int(input("What difficulty do you pick (1-4)?: "))
+   return selection
 
-def game():
-    print("Staring game of Hangman...")
-    wrongGuesses  = input("How many incorrect attempts do you want? ")
-    print("Selecting a word...")
-    print("Word selected.")
-
-    
+def game(mode):
+   
+    modes = {1:9, 2:7, 3:5} # Matches mode to guesses
+    guesses = modes[mode]
+    wrongGuesses = 0
     listOfGuesses = []
+    # print(randomWord) Dont't print the solution!!
 
-    to_guess = random.choice(fruits)
+    # Get a random word which would be guessed by the user
+    to_guess = random.choice(hangmanWords)
     to_guess = to_guess.lower()
 
-    guessed  = "#"*len(to_guess) #e.g cat -> ###
-    
-    while(wrongGuesses != guessed):
+    # The correctly guessed part of the word that we print after every guess
+    guessed  = "#"*len(to_guess) # e.g. "Hangman" --> "#######"
+    while(wrongGuesses != guesses):
         x = input("Word - %s . Guess a letter: "%guessed).lower()
         if x in to_guess:
             print(x,"is in the word!")
@@ -50,13 +62,17 @@ def game():
             listOfGuesses.append(x)
             print("Letters guessed so far:", listOfGuesses, "\n")
 
-        print("You lost the game!")
-        print("The word was %s"%to_guess)
-        return False # return false on loosing
+    print("You lost the game!")
+    print("The word was %s"%to_guess)
+    return False # return false on loosing
 
 
 def main():
-    game()
+   select = menu()
+   while(select != 4):
+      game(select)
+      select = menu()
 
+   print("You don't want to play today? :'(")
 
 main()
